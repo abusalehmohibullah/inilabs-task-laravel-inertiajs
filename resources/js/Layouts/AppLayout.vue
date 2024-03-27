3a6135b4af6e2cbdde9254c48640093f8d553dcb
Abu Saleh Mohibullah <abusalehmohibullah@gmail.com>
2024-03-27 13:09:39 +0600

    Installed InertiaJs (Vue3)

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+
+</script>
+
+<template>
+    <div>
+
+        <Head :title="title" />
+
+        <div class="bg-gray-100">
+
+            <slot />
+
+        </div>
+    </div>
+</template>
+
